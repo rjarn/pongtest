e8875967757c1b1936538cd25134b9b0e65160ec
rjarn <111002047+rjarn@users.noreply.github.com>
2023-04-21 00:57:16 -0400

    2nd

---
+++ scripts/ball.gd
@@ -0,0 +1,17 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func _ready():
+	velocity = Vector2(2000, 2000)
+	pass
+
+func _physics_process(delta):
+	var collision_info = move_and_collide(velocity * delta)
+	if collision_info:
+		velocity = velocity.bounce(collision_info.get_normal())
+	
